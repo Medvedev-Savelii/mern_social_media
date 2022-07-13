eec3ddfe71b221d8813bb46bce667cc28001336c
sava <savamedvedev@gmail.com>
2022-07-14 06:17:33 +0800

    3-commit

---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Profile.css";
+const Profile = () => {
+  return (
+    <div className="Profile">
+      {/* <ProfileLeft />
+      <div className="Profile-center">
+        <ProfileCard />
+        <PostSide />
+      </div>
+      <RightSide /> */}
+    </div>
+  );
+};
+
+export default Profile;
