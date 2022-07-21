962cede42b7879c43e4fdf06dfaead1a6ed280f5
sava <savamedvedev@gmail.com>
2022-07-21 14:47:41 +0800

    9-commit-Integration

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,18 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
-import "./Posts.css";
 import Post from "../Post/Post";
 import { useEffect } from "react";
 import { getTimelinePosts } from "../../actions/PostsAction";
+import { useParams } from "react-router-dom";
+import "./Posts.css";
 
 const Posts = () => {
+  const params = useParams();
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.authReducer.authData);
-  const { posts, loading } = useSelector((state) => state.postReducer);
+  let { posts, loading } = useSelector((state) => state.postReducer);
 
   useEffect(() => {
     dispatch(getTimelinePosts(user._id));
   }, []);
-
+  if (!posts) return "No Posts";
+  if (params.id) posts = posts.filter((post) => post.userId === params.id);
   return (
     <div className="Posts">
       {loading
